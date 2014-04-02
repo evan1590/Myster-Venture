af4adb779f5963d3810dda1662a5394104fe9f1f
Clayton Rieck <cjrieck123@gmail.com>
2014-04-02 16:28:54 -0400

    updating text file

--- story_tree.py
+++ story_tree.py
@@ -4,14 +4,17 @@ class Node(object):
 	"""docstring for Node"""
 	def __init__(self, item, right=None, left=None, straight=None, back=None):
 		self.cargo = item
-		self.right = right
-		self.left = left
-		self.straight = straight
-		self.back = back
+		self.directions = {
+			'right': right,
+			'left': left,
+			'straight': straight,
+			'back': back
+		}
 
 class StoryTree(object):
 	"""docstring for StoryTree"""
 	def __init__(self, root):
 		self.root = root
+	
+	def insert(self, string):
 		
-
