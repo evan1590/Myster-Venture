6318a88b657677664b945937db1d86a63961b134
Clayton Rieck <cjrieck123@gmail.com>
2014-04-15 20:58:11 -0400

    documented

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ from story_tree import *
 DIRECTIONS = ['right', 'left', 'straight', 'back']
 
 # preorder traversal to check the contents of
-# the tree
+# the tree. TESTING ONLY
 def check_tree(node):
 
 	if node:
@@ -28,11 +28,14 @@ def main():
 	story = [s.strip() for s in story.splitlines()]
 
 	game_tree = StoryTree()
+
 	# populate tree here
 	for step in story:
 		game_tree.insert(step)
 
-	check_tree(game_tree.root)
+	# prints out the tree's content
+	# FOR TESTING PURPOSES
+	# check_tree(game_tree.root)
 
 
 	atEnd = False
@@ -45,8 +48,10 @@ def main():
 
 	while (not atEnd):
 
-		if type(currentNode) == 'str':
+		if isinstance(currentNode, str):
+			print currentNode
 			atEnd = True
+
 		else:
 			print currentNode.cargo
 			print
@@ -60,29 +65,48 @@ def main():
 			# using a lookup table 
 			command = command.translate(string.maketrans("",""), string.punctuation)
 
+			# keeps track of each word of the command and
+			# all of their respective Synsets
 			commandDict = {}
 
+			# direction user will move
 			directionToMove = ""
 
 			# Creates a Thesaurus of words in the command string
-			# takes a long time
+			# ------- takes a long time -------
 			for word in command.lower().split():
+
+				# check if word in dictionary
 				if not commandDict.has_key(word):
+
+					# generate synsets for the next word in the command
 					synsets = WN.synsets(word)
 					
+					# get the lemmas for the Synsets generated
 					for lemma in synsets:
+
+						# iterate through DIRECTIONS
 						for direction in DIRECTIONS:
+
+							# check if direction in DIRECTIONS matches a word in
+							# the generated lemmas
 							if direction in synsets[0].lemma_names:
+
+								# if it does, then it's a direction and that's the direction
+								# the user wants to go
 								directionToMove = direction
 								break
-					# print synsets
 
+					# insert the synsets of the word if synsets exist
+					# for that word
 					if len(synsets) > 0:
 						commandDict[word] = WN.synsets(word)
 
 
 			# once we get Thesaurus, want to get 
 			# directions from the synsets
+
+			# move down the tree
 			currentNode = game_tree.step_next(currentNode, directionToMove)
 
 if __name__ == '__main__':
