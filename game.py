3dc69411edeec5b26b3a9657b17995a8924e717e
Clayton Rieck <cjrieck123@gmail.com>
2014-04-15 20:44:06 -0400

    worked on checking thesaurus of synsets against possible directions

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ import string
 
 from story_tree import *
 
+DIRECTIONS = ['right', 'left', 'straight', 'back']
+
 # preorder traversal to check the contents of
 # the tree
 def check_tree(node):
@@ -39,31 +41,49 @@ def main():
 
 	print intro 
 	
+	currentNode = game_tree.root
+
 	while (not atEnd):
 
-		command = raw_input("What will you do? : ")
+		if type(currentNode) == 'str':
+			atEnd = True
+		else:
+			print currentNode.cargo
+			print
 
-		# strip out all punctuation REALLY fast
-		# translate() uses raw string operations in C 
-		# using a lookup table 
-		command = command.translate(string.maketrans("",""), string.punctuation)
+			command = raw_input("What will you do? : ")
 
-		commandDict = {}
+			print
 
-		# Creates a Thesaurus of words in the command string
-		# takes a long time
-		for word in command.lower().split():
-			if not commandDict.has_key(word):
-				synsets = WN.synsets(word)
-				
-				# print synsets
+			# strip out all punctuation REALLY fast
+			# translate() uses raw string operations in C 
+			# using a lookup table 
+			command = command.translate(string.maketrans("",""), string.punctuation)
 
-				if len(synsets) > 0:
-					commandDict[word] = WN.synsets(word)
+			commandDict = {}
+
+			directionToMove = ""
+
+			# Creates a Thesaurus of words in the command string
+			# takes a long time
+			for word in command.lower().split():
+				if not commandDict.has_key(word):
+					synsets = WN.synsets(word)
+					
+					for lemma in synsets:
+						for direction in DIRECTIONS:
+							if direction in synsets[0].lemma_names:
+								directionToMove = direction
+								break
+					# print synsets
+
+					if len(synsets) > 0:
+						commandDict[word] = WN.synsets(word)
 
 
-		# once we get Thesaurus, want to get 
-		# directions from the synsets
+			# once we get Thesaurus, want to get 
+			# directions from the synsets
+			currentNode = game_tree.step_next(currentNode, directionToMove)
 
 if __name__ == '__main__':
 	main()
