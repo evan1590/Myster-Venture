bb6527cbb09de2c5a2372f06630d778cbee0c0dc
Clayton Rieck <cjrieck123@gmail.com>
2014-04-23 13:36:37 -0400

    Added graph

--- game.py
+++ game.py
@@ -5,6 +5,24 @@ import string
 
 from story_tree import *
 
+CAMPUS = {
+			'A': ['B','C'] # Young
+			'B': ['C','D','E'] # Clark
+			'C': ['A','B','D','N'] # Meadows
+			'D': ['B','C','E'] # Chase
+			'E': ['D','F'] # Meneely/Watson Courtyard
+			'F': ['M','G'] # Mary Lyon
+			'G': ['H','I','J'] # Dimple
+			'H': ['G'] # Emerson Dining
+			'I': ['G'] # Chapel
+			'J': ['K','L','G'] # Library
+			'K': ['L','J'] # New SC
+			'L': ['K','J'] # Old SC
+			'M': ['F','G'] # Park Hall
+			'N': ['K','J','C'] # Power Plant
+			'O': [] # WHALE
+		 }
+
 DIRECTIONS = {'right':['right'],
 			  'left':['left'], 
 			  'straight':['straight', 'forward'], 
